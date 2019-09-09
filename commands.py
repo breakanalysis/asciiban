261465f11d8273239eadc96ed332b6ffe2e4d6c5
Jacob Sznajdman <breakanalysis@gmail.com>
2019-09-09 19:33:14 +0200

    reorder methods and small fix

--- commands.py
+++ commands.py
@@ -6,11 +6,13 @@ from datetime import timedelta as td
 from render import render_kanban
 from constants import TITLE, CREATED, DATE_FORMAT, ID, STATUS, BACKLOG
 
-def get_issue_dir():
-    issue_dir = os.path.join(os.getcwd(), 'issues')
+def issues():
+    return [parse_issue_file(filename) for filename in issue_files()]
 
-def get_issue_path():
-    return os.path.join(get_issue_dir(), str(issue[ID]))
+def parse_issue_file(filename):
+    with open(filename, 'r') as f:
+        issue = json.load(f)
+    return issue
 
 def issue_files():
     issue_dir = get_issue_dir()
@@ -18,16 +20,25 @@ def issue_files():
         return []
     return [filename for filename in glob.iglob(os.path.join(issue_dir, '**'), recursive=True) if os.path.isfile(filename)]
 
-def parse_issue_file(filename):
-    with open(filename, 'r') as f:
-        issue = json.load(f)
-    return issue
+def get_issue_dir():
+    return os.path.join(os.getcwd(), 'issues')
 
-def issues():
-    return [parse_issue_file(filename) for filename in issue_files()]
+def get_issue_path():
+    return os.path.join(get_issue_dir(), str(issue[ID]))
+
+def write_issue(issue, issue_path):
+    with open(issue_path, 'w') as f:
+        json_str = json.dumps(issue, default=json_convert, indent=2)
+        f.write(json_str)
 
 def unescape(lambda_body):
     return lambda_body
+
+def format_date(date):
+    return date.strftime(DATE_FORMAT)
+
+def parse_date(date_str):
+    return dt.strptime(date_str, DATE_FORMAT)
 
 def matching_issues(query_body):
     matcher = compile_lambda(query_body)
@@ -39,6 +50,14 @@ def compile_lambda(query_body):
 def apply_udf(function_body, x):
     return exec(unescape(function_body))
             
+def id_from_filename(filename):
+    base = os.path.basename(filename)
+    return int(os.path.splitext(base)[0])
+
+def json_convert(o):
+    if isinstance(o, dt):
+        return format_date(o)
+
 def show_cmd(query_body):
     for line in render_kanban(matching_issues(query_body)):
         print(line)
@@ -48,20 +67,6 @@ def show_issues_cmd(query_body):
     for issue in matching_issues(query_body):
         print(issue)
         print(100 * '*')
-
-def format_date(date):
-    return date.strftime(DATE_FORMAT)
-
-def parse_date(date_str):
-    return dt.strptime(date_str, DATE_FORMAT)
-
-def id_from_filename(filename):
-    base = os.path.basename(filename)
-    return int(os.path.splitext(base)[0])
-
-def json_convert(o):
-    if isinstance(o, dt):
-        return format_date(o)
 
 def create_cmd(title, lambda_body=None):
     ids = [id_from_filename(filename) for filename in issue_files()]
@@ -76,17 +81,12 @@ def create_cmd(title, lambda_body=None):
     issue_path = os.path.join(issue_dir, str(id))
     write_issue(issue, issue_path)
 
-def write_issue(issue, issue_path):
-    with open(issue_path, 'w') as f:
-        json_str = json.dumps(issue, default=json_convert, indent=2)
-        f.write(json_str)
-
-def update_cmd(query_body, lambda_body):
-    for issue in matching_issues(query_body):
-        apply_udf(lambda_body, issue)
-        write_issue(issue, get_issue_path(issue)
-
 def delete_cmd(query_body):
     for issue in matching_issues(query_body):
         path = get_issue_path(issue)
         os.remove(path)
+
+def update_cmd(query_body, lambda_body):
+    for issue in matching_issues(query_body):
+        apply_udf(lambda_body, issue)
+        write_issue(issue, get_issue_path(issue))
